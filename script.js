98af59a155581fbe8bcb0b607cc220cddb06edcc
AbdulSami-af <Rafaybeast901@gmail.com>
2024-09-08 02:18:01 -0700

    title, description

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-// var counter = 1;
-// setInterval(function(){
-//     document.getElementById("radio" + counter).checked = true;
-//     counter++;
-//     if (counter > 7) {
-//         counter = 1;
-//     }
-// }, 3000)
+let menu = document.querySelector("#menu")
+let cross = document.querySelector("#cross")
+menu.addEventListener('click', () => {
+  menu.style.display = "none"
+  cross.style.display = "block"
+  gsap.to(cross, {
+    opacity: 1,
+    x: -6,
+    duration: 0.3
+  })
+  let tl = gsap.timeline()
+
+tl.to('.hidden',{
+  y: 390,
+  duration: 0.6,
+})
+ tl.to('.hidden a',{
+  opacity: 1,
+  stagger: 0.1,
+  y: 5
+})
+})
+
+cross.addEventListener('click', () => {
+  menu.style.display = "block"
+  cross.style.display = "none"
+  gsap.to(cross,{
+    x: 6
+  })
+  gsap.to(menu, {
+    opacity: 1,
+    duration: 0.3
+  })
+  let tl = gsap.timeline()
+  tl.to('.hidden', {
+    y: 0,
+    duration: 0.6
+  })
+  tl.to('.hidden a', {
+    opacity: 0,
+    stagger: 0.1
+  })
+})
